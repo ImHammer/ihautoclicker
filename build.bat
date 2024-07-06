10d6f02327d2ad6d39179a3ce72d6639392ee0de
ImHammer <hammerdevofc@gmail.com>
2024-07-05 22:29:48 -0300

    Transformando em apenas um arquivo de build

--- build.bat
+++ build.bat
@@ -4,25 +4,57 @@
 
 set this=%~dp0
 
-echo " ---> Gerando o runtime..."
-call %this%\\scripts\\genruntime.bat
+set ARCH=x86_64
+set req_lib_file=%this%\req_lib\windows\%ARCH%\JNativeHook.dll
 
-set this=%~dp0
+@rem GERANDO O RUNTIME IMAGE
+echo "---> Gerando o IHAutoClicker Runtime Image"
+@REM call gradlew.bat build jlink
 
-set portable_dir=%this%\\windows_portable
+if %ERRORLEVEL% neq 0 goto fail
 
-set runtime_dir=%this%\\app\\build\\ihautoclicker
-set target_dir=%portable_dir%\\app
-set exec_file=%target_dir%\\bin\\com.github.imhammer.ihautoclicker.bat
+@rem Limpando o console
+cls
 
-echo " ---> Criando o aplicativo portavel"
+@rem Movendo o runtime para a pasta destino
+set runtime_dir=%this%\app\build\ihautoclicker
+set portable_dir=%this%\windows_portable
+set app_dir=%portable_dir%\app
 
-if not exist "%portable_dir%" (
-    mkdir %portable_dir%
+echo "---> Movendo o runtime image para o local de destino"
+
+if not exist %runtime_dir% (
+    echo "!!! A pasta do runtime (%runtime_dir%) não foi encontrada"
+    goto fail
+)
+if exist %app_dir% (
+    rm %app_dir%
 )
 
-move %runtime_dir% %target_dir%
-cd %portable_dir%
+Move %runtime_dir% %app_dir%
 
-mklink /h run.bat .\\app\\bin\\com.github.imhammer.ihautoclicker.bat
+@rem Limpando o console
+cls
 
+@rem Copiando a livraria necessaria para o IHAutoClicker
+set app_bin_dir=%app_dir%\bin
+
+echo "---> Injetando a livraria JNativeHook"
+if not exist %req_lib_file% (
+    echo "!!! Arquivo %req_lib_file% não foi encontrado, ou o arquivo foi deletado"
+    goto fail
+)
+
+Copy %req_lib_file% %app_bin_dir%
+
+goto end
+
+:fail
+echo "!!! Ocorreu um error ao construir o aplicativo"
+pause
+exit 1
+
+:end
+echo " === SUCESSO ==="
+pause
+exit 0
